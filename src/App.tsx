64e636be0692a43f2f006f8bc86b35df4b3c84a7
kachourmehdi <125712746+KACHOURElmehdi@users.noreply.github.com>
2024-09-05 13:12:04 +0100

    Implement skeleton structure for responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <Button colorScheme='blue'>Button</Button>
-  )
-}
+    <Grid
+      templateAreas={{
+        base: `"header" "main"`,
+        lg: `"header header" "nav main" `,
+      }}
+    >
+      <GridItem area="header" bg="blue.500">
+        header
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="nav" bg="red.500">
+          nav
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="yellow.500">
+        main
+      </GridItem>
+    </Grid>
+  );
+};
 
-export default App
+export default App;
