ab88b25b9b93eab753f04d177cb17e55b7a1eca2
kachourmehdi <125712746+KACHOURElmehdi@users.noreply.github.com>
2024-10-01 23:54:06 +0100

    platform Selector Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformIconList from "./components/PlatformIconList";
+import PlatformSelector from "./components/PlatformSelector";
 
 const App = () => {
 
@@ -32,6 +34,7 @@ const App = () => {
         </GridItem>
       </Show>
       <GridItem area="main">
+        <PlatformSelector />
         <GameGrid selectedGenre={selectedGenre}/>
       </GridItem>
     </Grid>
